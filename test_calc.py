280682084f8062e1e1b0ba76e02dae8e8cfe4128
Ademar G <ademar.gamero@gmail.com>
2024-06-21 17:38:41 +0000

    add unit-tests, add auto-unitesting yaml file

---
+++ test_calc.py
@@ -0,0 +1,22 @@
+import unittest
+from calc import Calc
+
+class TestCalc(unittest.TestCase):
+
+    def setUp(self):
+        self.calc = Calc()
+
+    def test_add(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+
+    def test_sub(self):
+        self.assertEqual(self.calc.sub(2, 3), -1)
+
+    def test_mul(self):
+        self.assertEqual(self.calc.mul(2, 3), 6)
+    
+    def test_division(self):
+        self.assertEqual(self.calc.div(4,2),2)
+
+    def test_add_negative(self):
+        self.assertEqual(self.calc.add(-1,-1),-2)
